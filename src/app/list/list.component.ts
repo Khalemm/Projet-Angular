9d5636153502d5a63bdd0840192278bf948d99fd
MARTINOLLI luca <khalemm.orlog@gmail.com>
2021-02-18 11:58:24 +0100

    first commit

---
+++ src/app/list/list.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { POSTS } from '../@shared/mock';
+import { Post } from '../@shared/models/post';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit {
+
+  collapsed: boolean;
+
+  @Input() VarPost : Post;
+
+  posts: Post[] = POSTS;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log("posts > ", this.posts);
+  }
+
+}
